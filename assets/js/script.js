dc625acc06901193f7b38f76bcb7fa9d85e44a03
Ammar Ammar <ammar257ammar@gmail.com>
2021-01-22 01:41:55 +0100

    generate permalink for query

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function() {
                 $('#cookieModal').modal('hide');
             });
         }
-        
+
         jQuery("#query-button").on("click",function(event){
             event.preventDefault();
 
@@ -32,6 +32,28 @@ jQuery(document).ready(function() {
 		jQuery("#fetch").on("click",function(){
             fetchExamples();
 		});
+
+        //---------------- Populate query from URL (if available) -----------------------
+
+        function findGetParameter(parameterName) {
+            var result = null,
+                tmp = [];
+            location.search
+                .substr(1)
+                .split("&")
+                .forEach(function (item) {
+                  tmp = item.split("=");
+                  if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+                });
+            return result;
+        }
+
+        var query = findGetParameter("q");
+        if(query != null){
+            editor.getDoc().setValue(query);
+        }
+
+        //----------------  END OF Populate query from URL (if available) -----------------------
 
 		//---------------- Search funcionality starts ------------------------
 
@@ -76,5 +98,43 @@ jQuery(document).ready(function() {
             var queryText = getPrefixes() + query;
             exportResults(jQuery("#endpoint").val(), query, "xml");
         });
-} );
+
+        jQuery("#generate-permalink").on("click",function(e){
+
+            e.preventDefault();
+
+            var query = editor.getDoc().getValue();
+            var queryText = getPrefixes() + query;
+
+            query = "?q="+encodeURIComponent(query);
+
+            var url = window.location.href.split('?')[0] + query;
+
+            url = "http://sparql.wikipathways.org"+query;
+            var accessToken = "b0021fe4839aefbc4e7967b3578443d9ea6e89bf";
+
+            var params = {
+                "long_url" : url
+            };
+
+            console.log(url);
+            $.ajax({
+                url: "https://api-ssl.bitly.com/v4/shorten",
+                cache: false,
+                dataType: "json",
+                method: "POST",
+                contentType: "application/json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+                },
+                data: JSON.stringify(params)
+            }).done(function(data) {
+                console.log(data.link);
+                $('#permalink-url').html("<a href=\""+data.link+"\" target=\"_blank\">"+data.link+"</a>");
+                $('#permalinkModal').modal();
+            }).fail(function(data) {
+                console.log(data);
+            });
+        });
+    });
 })(jQuery);
